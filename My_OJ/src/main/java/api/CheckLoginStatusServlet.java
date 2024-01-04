77a6427ee18219627b9fd5492919de4b4580188e
tang-jy <114970379+cyan404-dot@users.noreply.github.com>
2024-01-05 05:15:03 +0800

    acac

--- My_OJ/src/main/java/api/CheckLoginStatusServlet.java
+++ My_OJ/src/main/java/api/CheckLoginStatusServlet.java
@@ -1,9 +1,12 @@
 package  api;
-
-import javax.servlet.*;
+import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.*;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
+import java.util.List;
 
 @WebServlet("/CheckLoginStatusServlet")
 public class CheckLoginStatusServlet extends HttpServlet {
